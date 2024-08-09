1239bd727c1b57f7664bde7d86ca3cfe41deedf0
SlovenskyUjo <adrianondik@azet.sk>
2024-08-09 16:48:44 +0200

    updated

--- resources/js/Components/Portfolio.tsx
+++ resources/js/Components/Portfolio.tsx
@@ -43,7 +43,7 @@ const Portfolio: React.FC = () => {
 
     return (
         <div className="max-w-[1000px] mx-auto p-6 md:my-20" id="portfolio">
-            <h2 className="text-3xl font-bold text-gray-200 mb-8 text-center flex justify-center items-center flex-col">
+            <h2 className="text-3xl font-bold text-gray-200 mb-8 text-center">
                 Portfolio
             </h2>
             {projects.map((project, index) => {
@@ -51,24 +51,27 @@ const Portfolio: React.FC = () => {
                 return (
                     <Reveal key={project.id}>
                         <div
-                            key={index}
                             className={`flex flex-col md:flex-row ${
                                 index % 2 !== 0 ? "md:flex-row-reverse" : ""
                             } mb-12`}
                         >
                             <div className="w-full md:w-1/2 p-4 flex-shrink-0 flex">
-                                <img
-                                    src={project.image}
-                                    alt={project.name}
-                                    className="w-full h-auto object-contain rounded-lg shadow-lg"
-                                />
+                                <div className="w-full h-[300px] overflow-hidden rounded-lg shadow-lg">
+                                    <img
+                                        src={project.image}
+                                        alt={project.name}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </div>
                             </div>
                             <div className="w-full md:w-1/2 p-4 flex flex-col justify-between">
-                                <div className="flex-grow">
+                                <div className="flex-grow flex flex-col">
                                     <h3 className="text-2xl font-semibold text-gray-200 mb-4">
                                         {project.name}
                                     </h3>
-                                    <p className="text-gray-300 mb-4">{project.description}</p>
+                                    <p className="text-gray-300 text-base mb-4">
+                                        {project.description}
+                                    </p>
                                     <p className="text-gray-400 mb-4">
                                         <span
                                             className={`px-3 py-1 rounded ${categoryColor} text-white`}
@@ -77,10 +80,10 @@ const Portfolio: React.FC = () => {
                                         </span>
                                     </p>
                                 </div>
-                                <div className="flex space-x-4 mt-4">
+                                <div className="flex space-x-2 mt-2">
                                     <a
                                         href={project.url}
-                                        className="px-4 py-2 bg-gray-600 text-gray-200 rounded-lg hover:bg-gray-700 transition duration-300"
+                                        className="px-3 py-2 text-sm bg-gray-500 text-gray-200 rounded-lg hover:bg-gray-600 transition duration-300"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
@@ -89,11 +92,11 @@ const Portfolio: React.FC = () => {
                                     {project.github_url && (
                                         <a
                                             href={project.github_url}
-                                            className="px-4 py-2 flex bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300"
+                                            className="px-3 py-2 text-sm flex items-center bg-slate-500 text-gray-200 rounded-lg hover:bg-slate-600 transition duration-300"
                                             target="_blank"
                                             rel="noopener noreferrer"
                                         >
-                                            <AiOutlineGithub className="text-xl mr-2 translate-y-0.5 " />
+                                            <AiOutlineGithub className="text-lg mr-1" />
                                             GitHub
                                         </a>
                                     )}
